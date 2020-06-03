364d704c5e8866b2ca7285aa4a9d67f2fb15b53b
Christophe DURAND <cdurand.dev@gmail.com>
2020-06-03 17:46:37 +0200

    instrumentalized tests implemented

---
+++ app/src/androidTest/java/com/christophedurand/mareu/meetings_list/MeetingsListTest.java
@@ -0,0 +1,245 @@
+package com.christophedurand.mareu.meetings_list;
+
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.christophedurand.mareu.ListMeetingActivity;
+import com.christophedurand.mareu.R;
+import com.christophedurand.mareu.utils.DeleteViewAction;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static com.christophedurand.mareu.utils.RecyclerViewItemCountAssertion.withItemCount;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class for list of meetings
+ */
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MeetingsListTest {
+    //-- PROPERTIES
+    private ListMeetingActivity mListMeetingActivity;
+
+    //-- RULE
+    @Rule
+    public ActivityTestRule<ListMeetingActivity> mListMeetingActivityRule =
+            new ActivityTestRule(ListMeetingActivity.class);
+
+    //-- SETUP
+    // INIT ACTIVITY
+    @Before
+    public void setUp() {
+        mListMeetingActivity = mListMeetingActivityRule.getActivity();
+
+        assertThat(mListMeetingActivity, notNullValue());
+    }
+
+    //-- UNIT TESTS METHODS
+    /**
+     * We test that a meeting is created and then is displayed in our recyclerview
+     */
+    @Test
+    public void myMeetingsList_createAction_shouldNotBeEmpty() {
+        // Given: We perform a click on add meeting button, it displays the add meeting activity layout
+        onView(ViewMatchers.withId(R.id.add_meeting) ).perform(ViewActions.click() );
+
+        // When: We type the topic of the meeting
+        onView(withId(R.id.topic) ).perform(ViewActions.click() );
+        onView(withId(R.id.topic) ).perform(ViewActions.typeText("Meeting 1"), ViewActions.closeSoftKeyboard() );
+        // We set the date of the meeting
+        onView(withId(R.id.date) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName() ) ) ).
+                perform(PickerActions.setDate(2020, 06, 03));
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click() );
+        // We set the time of the meeting
+        onView(withId(R.id.time) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName() ) ) ).
+                perform(PickerActions.setTime(11, 30) );
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click() );
+        // We set the place of the meeting
+        onView(withId(R.id.place) ).perform(ViewActions.click() );
+        onView(withId(R.id.radiobutton_roomC) ).perform(ViewActions.click() );
+        onView(withId(R.id.button_ok) ).perform(ViewActions.click() );
+        // We type the participants of the meeting
+        onView(withId(R.id.participants) ).perform(ViewActions.click() );
+        onView(withId(R.id.participants) ).perform(ViewActions.typeText("c.durand@gmail.com"),
+                ViewActions.closeSoftKeyboard() );
+        // We perform a click on the create button to add the meeting into our list
+        onView(withId(R.id.create) ).perform(ViewActions.click() );
+
+        // Then: the meeting is displayed into our recyclerview
+        onView(withId(R.id.recycler_view_meetings) ).check(withItemCount(1) );
+    }
+
+    /**
+     * When we delete an item, the item is no more shown
+     */
+    @Test
+    public void myMeetingsList_deleteAction_shouldRemoveItem() {
+        // Given: We perform a click on add meeting button, it displays the add meeting activity layout
+        onView(withId(R.id.add_meeting) ).perform(ViewActions.click() );
+
+        // When: We type the topic of the meeting
+        onView(withId(R.id.topic) ).perform(ViewActions.click() );
+        onView(withId(R.id.topic) ).perform(ViewActions.typeText("Meeting to delete"), ViewActions.closeSoftKeyboard() );
+        // We set the date of the meeting
+        onView(withId(R.id.date) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName() ) ) ).
+                perform(PickerActions.setDate(2020, 06, 03));
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click());
+        // We set the time of the meeting
+        onView(withId(R.id.time) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName() ) ) ).
+                perform(PickerActions.setTime(11, 30) );
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click());
+        // We set the place of the meeting
+        onView(withId(R.id.place) ).perform(ViewActions.click() );
+        onView(withId(R.id.radiobutton_roomC) ).perform(ViewActions.click() );
+        onView(withId(R.id.button_ok) ).perform(ViewActions.click() );
+        // We type the participants of the meeting
+        onView(withId(R.id.participants) ).perform(ViewActions.click() );
+        onView(withId(R.id.participants) ).perform(ViewActions.typeText("c.durand@gmail.com"),
+                ViewActions.closeSoftKeyboard() );
+        // We perform a click on the create button to add the meeting into our list
+        onView(withId(R.id.create) ).perform(ViewActions.click() );
+        // The meeting is displayed into our recyclerview
+        onView(withId(R.id.recycler_view_meetings) ).check(withItemCount(2) );
+        // We perform a click on the delete button
+        onView(withId(R.id.recycler_view_meetings) )
+                .perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction() ) );
+
+        // Then: It removes the meeting from the list
+        onView(withId(R.id.recycler_view_meetings) ).check(withItemCount(1) );
+
+    }
+
+    /**
+     * When we tapped the filter date radio button, only the filtered item is shown
+     */
+    @Test
+    public void myMeetingsList_filterAction_shouldFilterItemsByDate() {
+        // SECOND MEETING CREATED
+        // Given: We perform a click on add meeting button, it displays the add meeting activity layout
+        onView(withId(R.id.add_meeting) ).perform(ViewActions.click() );
+        // We type the topic of the meeting
+        onView(withId(R.id.topic) ).perform(ViewActions.click() );
+        onView(withId(R.id.topic) ).perform(ViewActions.typeText("Meeting 2"), ViewActions.closeSoftKeyboard() );
+        // We set the date of the meeting
+        onView(withId(R.id.date) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName() ) ) ).
+                perform(PickerActions.setDate(2020, 06, 04));
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click());
+        // We set the time of the meeting
+        onView(withId(R.id.time) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName() ) ) ).
+                perform(PickerActions.setTime(10, 30) );
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click());
+        // We set the place of the meeting
+        onView(withId(R.id.place) ).perform(ViewActions.click() );
+        onView(withId(R.id.radiobutton_roomD) ).perform(ViewActions.click() );
+        onView(withId(R.id.button_ok) ).perform(ViewActions.click() );
+        // We type the participants of the meeting
+        onView(withId(R.id.participants) ).perform(ViewActions.click() );
+        onView(withId(R.id.participants) ).perform(ViewActions.typeText("christophe.dd@gmail.com"),
+                ViewActions.closeSoftKeyboard() );
+        // We perform a click on the create button to add the meeting into our list
+        onView(withId(R.id.create) ).perform(ViewActions.click() );
+
+        // When: We perform a click on the filter button
+        onView(withId(R.id.filter_image_button) ).perform(ViewActions.click() );
+        // We set the filter by date
+        onView(withId(R.id.radiobutton_date) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName() ) ) ).
+                perform(PickerActions.setDate(2020, 06, 03));
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click() );
+
+        // Then: It only displays the filtered meeting into the list
+        onView(withId(R.id.recycler_view_meetings) ).check(withItemCount(1) );
+    }
+
+    /**
+     * When we tapped the filter place radio button, only the filtered item is shown
+     */
+    @Test
+    public void myMeetingsList_filterAction_shouldFilterItemsByPlace() {
+        // Given: We perform a click on the filter button
+        onView(withId(R.id.filter_image_button) ).perform(ViewActions.click() );
+
+        // When: We set the filter by place
+        onView(withId(R.id.radiobutton_place) ).perform(ViewActions.click() );
+        onView(withId(R.id.radiobutton_roomD) ).perform(ViewActions.click() );
+        onView(withId(R.id.button_ok) ).perform(ViewActions.click() );
+
+        // Then: It only displays the filtered meeting into the list
+        onView(withId(R.id.recycler_view_meetings) ).check(withItemCount(1) );
+    }
+
+    /**
+     * When we tapped the no filter radio button, all the items are shown
+     */
+    @Test
+    public void myMeetingsList_filterAction_shouldNotFilterItems() {
+        // Given: We perform a click on the filter button
+        onView(withId(R.id.filter_image_button) ).perform(ViewActions.click() );
+        // We set the filter by date
+        onView(withId(R.id.radiobutton_date) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName() ) ) ).
+                perform(PickerActions.setDate(2020, 06, 03));
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click() );
+        // We perform a click on the filter button
+        onView(withId(R.id.filter_image_button) ).perform(ViewActions.click() );
+        // We set the filter by place
+        onView(withId(R.id.radiobutton_place) ).perform(ViewActions.click() );
+        onView(withId(R.id.radiobutton_roomD) ).perform(ViewActions.click() );
+        onView(withId(R.id.button_ok) ).perform(ViewActions.click() );
+
+        // When: We perform a click on the filter button
+        onView(withId(R.id.filter_image_button) ).perform(ViewActions.click() );
+        // We don't set any filter
+        onView(withId(R.id.radiobutton_without_filter) ).perform(ViewActions.click() );
+
+        // Then: It displays all the meetings into the list
+        onView(withId(R.id.recycler_view_meetings) ).check(withItemCount(2) );
+    }
+
+    /**
+     * When we tapped the item delete button, while the items are filtered,
+     * it only removes the selected item and updates dynamically the meetings list
+     */
+    @Test
+    public void myMeetingsList_filterAndDeleteActions_shouldUpdateList() {
+        // Given: We perform a click on the filter button
+        onView(withId(R.id.filter_image_button) ).perform(ViewActions.click() );
+        // We set the filter by date
+        onView(withId(R.id.radiobutton_date) ).perform(ViewActions.click() );
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName() ) ) ).
+                perform(PickerActions.setDate(2020, 06, 04));
+        onView(withId(android.R.id.button1) ).perform(ViewActions.click() );
+
+        // When: We perform a click on the delete button
+        onView(withId(R.id.recycler_view_meetings) )
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, new DeleteViewAction() ) );
+
+        // Then: It displays all the meetings into the list
+        onView(withId(R.id.recycler_view_meetings) ).check(withItemCount(1) );
+    }
+
+}
--- app/src/main/java/com/christophedurand/mareu/ListMeetingActivity.java
+++ app/src/main/java/com/christophedurand/mareu/ListMeetingActivity.java
@@ -1,52 +1,107 @@
 package com.christophedurand.mareu;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.christophedurand.mareu.DI.DI;
 import com.christophedurand.mareu.Model.Meeting;
 import com.christophedurand.mareu.Service.MeetingApiService;
-import com.christophedurand.mareu.UI.MeetingFragment;
+import com.christophedurand.mareu.UI.ListMeetingsInterface;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-public class ListMeetingActivity extends AppCompatActivity {
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class ListMeetingActivity extends AppCompatActivity implements ListMeetingsInterface {
     //-- PROPERTIES
-    MeetingApiService mApiService;
-    MeetingFragment meetingFragment;
-    List<Meeting> listMeetings = new ArrayList<>();
+    private MeetingApiService mApiService;
+    private List<Meeting> mMeetings;
+    // UI
+    private RecyclerView mRecyclerView;
+    private MyMeetingRecyclerViewAdapter myMeetingRecyclerViewAdapter;
+    // CALENDAR
+    private final Calendar myCalendar = Calendar.getInstance();
+    // DATE
+    private DatePickerDialog.OnDateSetListener date = (view, year, monthOfYear, dayOfMonth) -> {
+        myCalendar.set(Calendar.YEAR, year);
+        myCalendar.set(Calendar.MONTH, monthOfYear);
+        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        mMeetings = mApiService.getMeetings();
+        initList();
+
+        List<Meeting> filteredMeetings = new ArrayList<>();
+
+        for (int i = 0; i<mMeetings.size(); i++) {
+            Meeting meetingFiltered = mMeetings.get(i);
+            Date meetingFilteredDate = meetingFiltered.getDate();
+            Date myCalendarFilteredDate = myCalendar.getTime();
+
+            String myFormat = "dd/MM/yyyy";
+            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.FRANCE);
+
+            String meetingFilteredDateString = sdf.format(meetingFilteredDate);
+            String myCalendarFilteredDateString = sdf.format(myCalendarFilteredDate);
+
+            if (meetingFilteredDateString.contains(myCalendarFilteredDateString) ) {
+                filteredMeetings.add(meetingFiltered);
+            }
+        }
+
+        mMeetings = filteredMeetings;
+        initList();
+        Collections.sort(mMeetings, Comparator.comparing(Meeting::getDate));
+    };
 
     //-- VIEW LIFE CYCLE
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // INIT INSTANCE OF API SERVICE
         mApiService = DI.getMeetingApiService();
-
-        listMeetings = mApiService.getMeetings();
-
+        // INIT LIST MEETINGS
+        mMeetings = mApiService.getMeetings();
+        // SET CONTENT VIEW WITH LAYOUT
         setContentView(R.layout.activity_main);
-
+        // BIND BUTTERKNIFE
         ButterKnife.bind(this);
+        // INIT RECYCLER VIEW LAYOUT
+        mRecyclerView = findViewById(R.id.recycler_view_meetings);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // SET DIVIDER ITEM DECORATION
+        DividerItemDecoration myDivider = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(myDivider);
+        // SET ADAPTER
+        myMeetingRecyclerViewAdapter = new MyMeetingRecyclerViewAdapter(mMeetings, this);
+        mRecyclerView.setAdapter(myMeetingRecyclerViewAdapter);
+    }
 
-        meetingFragment = MeetingFragment.newInstance(listMeetings);
-
-        initFragment(meetingFragment);
+    @Override
+    public void onResume() {
+        super.onResume();
+        initList();
     }
 
     //-- ON CLICK
@@ -55,7 +110,6 @@ public class ListMeetingActivity extends AppCompatActivity {
         AddMeetingActivity.navigate(this);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     @OnClick(R.id.filter_image_button)
     void showFilterRadioButtonDialog() {
         Dialog dialog = new Dialog(this);
@@ -70,19 +124,93 @@ public class ListMeetingActivity extends AppCompatActivity {
 
         dialog.show();
 
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                meetingFragment.filterListButtonIsTapped(radioButtonDate, radioButtonPlace, radioButtonWithoutFilter);
-                dialog.dismiss();
-            }
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            filterListButtonIsTapped(radioButtonDate, radioButtonPlace, radioButtonWithoutFilter);
+            dialog.dismiss();
         });
 
         buttonOk.setOnClickListener(v -> dialog.dismiss());
     }
 
     //-- METHODS
-    public void initFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_fragment_meeting, fragment).commit();
+    /**
+     * Action of delete meeting button
+     */
+    @Override
+    public void onDeleteMeeting(Meeting meeting) {
+        mMeetings = mApiService.getMeetings();
+        mMeetings.remove(meeting);
+        initList();
+    }
+
+    /**
+     * Init the list of meetings
+     */
+    private void initList() {
+        myMeetingRecyclerViewAdapter = new MyMeetingRecyclerViewAdapter(mMeetings, this);
+        mRecyclerView.setAdapter(myMeetingRecyclerViewAdapter);
+    }
+
+    /**
+     * Update data in the list of meetings and recycler view adapter
+     */
+    private void filterListButtonIsTapped(RadioButton dateRadioButton, RadioButton placeRadioButton,
+                                         RadioButton withoutFilterRadioButton) {
+        if (dateRadioButton.isChecked() ) {
+            placeRadioButton.setChecked(false);
+            withoutFilterRadioButton.setChecked(false);
+
+            new DatePickerDialog(this, R.style.DialogTheme, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+        } else if (placeRadioButton.isChecked() ) {
+            dateRadioButton.setChecked(false);
+            withoutFilterRadioButton.setChecked(false);
+
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.radiobutton_place_dialog);
+            dialog.setCancelable(true);
+
+            RadioGroup radioGroup = dialog.findViewById(R.id.radio_group_places);
+            Button buttonOk = dialog.findViewById(R.id.button_ok);
+
+            dialog.show();
+
+            mMeetings = mApiService.getMeetings();
+            initList();
+
+            radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+                RadioButton radioButtonRoom = dialog.findViewById(checkedId);
+
+                int tag = Integer.parseInt((String) (radioButtonRoom.getTag() ) );
+
+                String titlePlace = getResources().getStringArray(R.array.places_titles)[tag];
+
+                List<Meeting> filteredMeetings = new ArrayList<>();
+
+                for (int i = 0; i<mMeetings.size(); i++) {
+                    Meeting meetingFiltered = mMeetings.get(i);
+                    String meetingPlace = meetingFiltered.getPlace();
+
+                    if (meetingPlace.equals(titlePlace) ) {
+                        filteredMeetings.add(meetingFiltered);
+                    }
+                }
+
+                mMeetings = filteredMeetings;
+                initList();
+                Collections.sort(mMeetings, Comparator.comparing(Meeting::getDate));
+            });
+
+            buttonOk.setOnClickListener(v -> dialog.dismiss());
+
+        } else if (withoutFilterRadioButton.isChecked()) {
+            placeRadioButton.setChecked(false);
+            dateRadioButton.setChecked(false);
+
+            mMeetings = mApiService.getMeetings();
+            initList();
+        }
     }
 
 }
